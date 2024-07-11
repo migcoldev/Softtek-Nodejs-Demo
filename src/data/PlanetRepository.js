ebb9de829abfd5e5f380aa6c3864e3cfcf298a1c
Miguel Collantes <miguel.collantes.cavero@gmail.com>
2024-07-11 01:15:17 -0500

    Hotfix

--- src/data/PlanetRepository.js
+++ src/data/PlanetRepository.js
@@ -1,6 +1,7 @@
 const db = require(`../../helpers/db`);
 const PlanetTable = process.env.PLANET_TABLE;
 const { _headers_cors } = require('../helper/utils')
+import { v4 as uuidv4 } from 'uuid';
 
 class PlanetRepository {
     constructor() {
@@ -12,7 +13,14 @@ class PlanetRepository {
             TableName: this.tableName
         };
 
-        return await db.scan(params).promise();
+        try {
+            const { Items } = await client.send(new ScanCommand(params));
+            return Items;
+        } catch (error) {
+            console.error("Error al obtener items de DynamoDB:", error);
+            throw error;
+        }
+        //return await db.scan(params).promise();
     }
 
     async create(data) {
@@ -20,11 +28,24 @@ class PlanetRepository {
             TableName: this.tableName,
             Item: {
                 PlanetId: uuidv4(),
-                Username: data.Username,
+                nombre: data.nombre,
+                clima: data.clima,
+                diametro: data.diametro,
+                gravedad: data.gravedad,
+                poblacion: data.poblacion,
+                periodo_rotacion: data.periodo_rotacion,
+                url: data.url
             },
         };
 
-        await db.put(params).promise();
+        try {
+            await this.client.send(new PutCommand(params));
+            return item;
+        } catch (error) {
+            console.error("Error DB : ", error);
+            throw error;
+        }
+        //await db.put(params).promise();
 
         return params.Item;
     }
